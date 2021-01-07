3013dd8d2e4925af28276c020c9625df18ae37f7
guying-01 <aries_man@126.com>
2021-01-07 17:14:20 +0800

    表格颜色、展开收起

--- src/components/FilterPanel/index.vue
+++ src/components/FilterPanel/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author       : gy
  * @Date         : 2021-01-06 21:41:59
- * @LastEditors  : gy
- * @LastEditTime : 2021-01-06 21:48:39
+ * @LastEditors: gy
+ * @LastEditTime: 2021-01-07 17:10:28
  * @FilePath     : /yufufei/src/components/FilterPanel/index.vue
  * @Description  : 页面描述
 -->
@@ -10,38 +10,59 @@
   <div>
     <div class="title">
       <div>条件筛选</div>
-      <div>收起</div>
+      <div @click="isFold = !isFold">{{ isFold ? '展开' : '收起' }}</div>
     </div>
-    <div class="content">
-      <slot/>
+    <div ref="content" class="content">
+      <div ref="wrap" class="wrap">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props : {},
-  data () {
+  components: {},
+  props: {},
+  data() {
     return {
+      isFold: false,
     };
   },
-  components: {},
-  mounted(){},
-  methods: {}
-}
-
+  watch: {
+    isFold: {
+      handler(val) {
+        let cont = this.$refs["content"];
+        if (val) {
+          cont.style.height = 0 + "px";
+        } else {
+          cont.style.height = cont.scrollHeight + "px";
+        }
+      },
+    },
+  },
+  mounted() {
+    let cont = this.$refs["content"];
+    cont.style.height = cont.scrollHeight + "px";
+  },
+  methods: {},
+};
 </script>
 <style lang='scss' scoped>
-.title{
+.title {
   display: flex;
   justify-content: space-between;
   cursor: pointer;
   border-bottom: 1px solid #e8e9ee;
   padding: 14px 25px;
-  color:#6b6c8f;
+  color: #6b6c8f;
 }
 
-.content{
-  padding: 20px;
+.content {
+  transition: all linear 0.5s;
+  overflow: hidden;
+  .wrap {
+    padding: 20px;
+  }
 }
 </style>
--- src/views/user-management/dianbiao-batch-operation/index.vue
+++ src/views/user-management/dianbiao-batch-operation/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author       : gy
  * @Date         : 2021-01-04 20:30:12
- * @LastEditors  : gy
- * @LastEditTime : 2021-01-06 22:10:00
+ * @LastEditors: gy
+ * @LastEditTime: 2021-01-07 16:42:31
  * @FilePath     : /yufufei/src/views/user-management/dianbiao-batch-operation/index.vue
  * @Description  : 页面描述
 -->
@@ -18,27 +18,26 @@
                 :props="defaultProps"
                 accordion
                 @node-click="handleNodeClick"
-              >
-              </el-tree>
+              />
             </el-col>
             <el-col :span="18">
-              <el-form :model="form" ref="form" size="mini" label-width="100px">
+              <el-form ref="form" :model="form" size="mini" label-width="100px">
                 <el-row>
                   <el-col :span="12">
                     <el-form-item label="剩余金额:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="全部"></el-radio>
-                        <el-radio label="低于或等于0"></el-radio>
-                        <el-radio label="大于0"></el-radio>
+                        <el-radio label="全部" />
+                        <el-radio label="低于或等于0" />
+                        <el-radio label="大于0" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
                   <el-col :span="12">
                     <el-form-item label="联网状态:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="全部"></el-radio>
-                        <el-radio label="在线"></el-radio>
-                        <el-radio label="失联"></el-radio>
+                        <el-radio label="全部" />
+                        <el-radio label="在线" />
+                        <el-radio label="失联" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
@@ -48,18 +47,18 @@
                   <el-col :span="12">
                     <el-form-item label="合闸状态:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="全部"></el-radio>
-                        <el-radio label="合闸"></el-radio>
-                        <el-radio label="拉闸"></el-radio>
+                        <el-radio label="全部" />
+                        <el-radio label="合闸" />
+                        <el-radio label="拉闸" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
                   <el-col :span="12">
                     <el-form-item label="电表模式:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="全部"></el-radio>
-                        <el-radio label="预付费模式"></el-radio>
-                        <el-radio label="强制模式"></el-radio>
+                        <el-radio label="全部" />
+                        <el-radio label="预付费模式" />
+                        <el-radio label="强制模式" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
@@ -69,18 +68,18 @@
                   <el-col :span="12">
                     <el-form-item label="开户状态:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="全部"></el-radio>
-                        <el-radio label="已开户"></el-radio>
-                        <el-radio label="未开户"></el-radio>
+                        <el-radio label="全部" />
+                        <el-radio label="已开户" />
+                        <el-radio label="未开户" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
                   <el-col :span="12">
                     <el-form-item label="告警状态:">
                       <el-radio-group v-model="form.resource">
-                        <el-radio label="报警1"></el-radio>
-                        <el-radio label="报警2"></el-radio>
-                        <el-radio label="欠费"></el-radio>
+                        <el-radio label="报警1" />
+                        <el-radio label="报警2" />
+                        <el-radio label="欠费" />
                       </el-radio-group>
                     </el-form-item>
                   </el-col>
@@ -118,9 +117,55 @@
         <el-button type="primary" size="mini">下发报警短信</el-button>
         <el-button type="primary" size="mini">刷新表状态</el-button>
         <el-button type="primary" size="mini">历史抄表记录</el-button>
-
       </el-button-group>
     </div>
+
+    <div class="list">
+      <div class="row-status">
+        <div class="item">
+          <div class="block ykh" />
+          <span>已开户</span>
+        </div>
+        <div class="item">
+          <div class="block bj1" />
+          <span>报警1</span>
+        </div>
+        <div class="item">
+          <div class="block bj2" />
+          <span>报警2</span>
+        </div>
+        <div class="item">
+          <div class="block qf" />
+          <span>欠费</span>
+        </div>
+        <div class="item">
+          <div class="block wkh" />
+          <span>未开户</span>
+        </div>
+        <div class="item">
+          <div class="block sl" />
+          <span>失联</span>
+        </div>
+        <div class="item">
+          <div class="block hbjs" />
+          <span>合并计算</span>
+        </div>
+      </div>
+    </div>
+
+    <el-table :data="tableData" :row-class-name="getRowClassName">
+      <el-table-column label="仪表编号" prop="field1" />
+      <el-table-column label="商铺号" prop="field2" />
+      <el-table-column label="用户编号" prop="field3" />
+      <el-table-column label="用户名" prop="field4" />
+      <el-table-column label="购电总额" prop="field5" />
+      <el-table-column label="购电次数" prop="field6" />
+      <el-table-column label="告警金额1" prop="field7" />
+      <el-table-column label="告警金额2" prop="field8" />
+      <el-table-column label="功率阈值(KW)" prop="field9" />
+      <el-table-column label="尖电价" prop="field10" />
+      <el-table-column label="峰电价" prop="field11" />
+    </el-table>
   </div>
 </template>
 
@@ -140,11 +185,11 @@ export default {
               label: "二级 1-1",
               children: [
                 {
-                  label: "三级 1-1-1"
-                }
-              ]
-            }
-          ]
+                  label: "三级 1-1-1",
+                },
+              ],
+            },
+          ],
         },
         {
           label: "一级 2",
@@ -153,19 +198,19 @@ export default {
               label: "二级 2-1",
               children: [
                 {
-                  label: "三级 2-1-1"
-                }
-              ]
+                  label: "三级 2-1-1",
+                },
+              ],
             },
             {
               label: "二级 2-2",
               children: [
                 {
-                  label: "三级 2-2-1"
-                }
-              ]
-            }
-          ]
+                  label: "三级 2-2-1",
+                },
+              ],
+            },
+          ],
         },
         {
           label: "一级 3",
@@ -174,26 +219,93 @@ export default {
               label: "二级 3-1",
               children: [
                 {
-                  label: "三级 3-1-1"
-                }
-              ]
+                  label: "三级 3-1-1",
+                },
+              ],
             },
             {
               label: "二级 3-2",
               children: [
                 {
-                  label: "三级 3-2-1"
-                }
-              ]
-            }
-          ]
-        }
+                  label: "三级 3-2-1",
+                },
+              ],
+            },
+          ],
+        },
       ],
       defaultProps: {
         children: "children",
-        label: "label"
+        label: "label",
       },
-      form: {}
+      form: {},
+      colorMap: {
+        1: "ykh",
+        2: "bj1",
+        3: "bj2",
+        4: "qf",
+        5: "wkh",
+        6: "sl",
+        7: "hbjs",
+      },
+      tableData: [
+        {
+          field1: "019047864",
+          field2: "001",
+          field3: "0190070",
+          field4: "未开户",
+          field5: "100.00",
+          field6: "1",
+          field7: "300.00",
+          field8: "45",
+          field9: "54",
+          field10: "1.0000",
+          field11: "1.0000",
+          status: 1,
+        },
+        {
+          field1: "019047864",
+          field2: "001",
+          field3: "0190070",
+          field4: "未开户",
+          field5: "100.00",
+          field6: "1",
+          field7: "300.00",
+          field8: "45",
+          field9: "54",
+          field10: "1.0000",
+          field11: "1.0000",
+          status: 2,
+        },
+        {
+          field1: "019047864",
+          field2: "001",
+          field3: "0190070",
+          field4: "未开户",
+          field5: "100.00",
+          field6: "1",
+          field7: "300.00",
+          field8: "45",
+          field9: "54",
+          field10: "1.0000",
+          field11: "1.0000",
+          status: 3,
+        },
+        {
+          field1: "019047864",
+          field2: "001",
+          field3: "0190070",
+          field4: "未开户",
+          field5: "100.00",
+          field6: "1",
+          field7: "300.00",
+          field8: "45",
+          field9: "54",
+          field10: "1.0000",
+          field11: "1.0000",
+          status: 4,
+        },
+      ],
     };
   },
   created() {},
@@ -201,9 +313,82 @@ export default {
   methods: {
     handleNodeClick(data) {
       console.log(data);
-    }
-  }
+    },
+    getRowClassName({ row }) {
+      return this.colorMap[row["status"]];
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.list {
+  .row-status {
+    display: flex;
+    justify-content: flex-end;
+    .item {
+      display: flex;
+      padding: 0 2px;
+      .block {
+        width: 15px;
+        height: 15px;
+        margin-right: 3px;
+        &.ykh {
+          background-color: lightblue;
+        }
+        &.bj1 {
+          background-color: #fff100;
+        }
+        &.bj2 {
+          background-color: #ec1616;
+        }
+        &.qf {
+          background-color: #a332d8;
+        }
+        &.wkh {
+          background-color: #adadad;
+        }
+        &.sl {
+          background-color: lightgreen;
+        }
+        &.hbjs {
+          background-color: #50a9e1;
+        }
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.el-table {
+  .ykh {
+    background-color: lightblue;
+    color: #fff;
+  }
+  .bj1 {
+    background-color: #fff100;
+    color: #fff;
+  }
+  .bj2 {
+    background-color: #ec1616;
+    color: #fff;
+  }
+  .qf {
+    background-color: #a332d8;
+    color: #fff;
+  }
+  .wkh {
+    background-color: #adadad;
+    color: #fff;
+  }
+  .sl {
+    background-color: lightgreen;
+    color: #fff;
+  }
+  .hbjs {
+    background-color: #50a9e1;
+    color: #fff;
+  }
+}
+</style>
